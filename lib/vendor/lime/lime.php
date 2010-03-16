8bdb4accfe369a4e6d412eec4954baffb519db1f
bmaron <bmaron@pc20-139.localdomain>
2010-03-16 14:57:38 +0100

    Be more TAP compliant

--- lib/vendor/lime/lime.php
+++ lib/vendor/lime/lime.php
@@ -558,12 +558,12 @@ class lime_output
 
   public function info($message)
   {
-    echo $this->colorizer->colorize(sprintf('> %s', $message), 'INFO_BAR')."\n";
+    echo $this->colorizer->colorize(sprintf('# > %s', $message), 'INFO_BAR')."\n";
   }
 
   public function error($message)
   {
-    echo $this->colorizer->colorize(sprintf(' %s ', $message), 'RED_BAR')."\n";
+    echo $this->colorizer->colorize(sprintf('# %s ', $message), 'RED_BAR')."\n";
   }
 
   public function echoln($message, $colorizer_parameter = null, $colorize = true)
